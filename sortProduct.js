f34a23bd383e36949628c826bfe6fa4c827911f0
Adam Cox <adamcox.dev@gmail.com>
2022-09-22 10:28:07 -0600

    Final commit

--- sortProduct.js
+++ sortProduct.js
@@ -25,3 +25,49 @@
 // Output: 2, 3, 4, 23, 5
 
 
+function sort(input) {
+    let newArr = [];
+    for (let i = 0; i < input.length; i++) {
+    newArr.push(input[i] * (i + 1)); //multiplying
+  
+     }
+  
+    //bubble  adjusted using our original input
+    for (let i = 0; i < newArr.length; i++) {
+      for (let j = 1; j < newArr.length; j++) {
+        if (newArr[j - 1] > newArr[i]) {
+          temp = newArr[j - 1];
+          temp2 = input [j-1];
+          newArr[j - 1] = newArr[i];
+          input[j-1] = input[i];
+          newArr[i] = temp;
+          input[i]=temp2;
+        }
+      }
+  
+    }
+    return input;
+  }
+  
+  // take original array, and put a 1 at the front to change the index
+  // then multiply this array by the number at it's index
+  
+  console.log(sort([23, 2, 3, 4, 5]))
+
+//_________________________________________________________________
+  
+
+function sortByValueAndIndex(array) {
+    let totals = []
+    array.forEach((e, i) => {
+      let totalObj = {
+        total: e*(i+1),
+        i: i
+      }
+      totals.push(totalObj)
+    })
+    totals.sort((a, b) => a.total - b.total)
+    let newArr = []
+    totals.forEach(e => newArr.push(array[e.i]))
+    return newArr
+  }
